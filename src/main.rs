82a25f866802613f6daa647f821737ca9fc1681d
shirokurostone <426017+shirokurostone@users.noreply.github.com>
2022-10-09 22:38:04 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use randstring::parser::generate_rand_string;
+use randstring::parser::root;
+
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().len() != 2 {
+        std::process::exit(1);
+    }
+
+    let status = match std::env::args().nth(1) {
+        Some(s) => match root(&s) {
+            Ok((token, _)) => match generate_rand_string(&token) {
+                Ok(string) => {
+                    print!("{}", string);
+                    0
+                }
+                Err(_) => 1,
+            },
+            Err(_) => 1,
+        },
+        _ => 1,
+    };
+    std::process::exit(status);
 }
