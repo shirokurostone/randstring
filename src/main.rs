d3833fd10c2fc856a5805f44d29d1edecb733ebe
shirokurostone <426017+shirokurostone@users.noreply.github.com>
2022-10-10 20:42:03 +0900

    Use clap

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 use randstring::parser::generate_rand_string;
 use randstring::parser::root;
 
-fn main() {
-    if std::env::args().len() != 2 {
-        std::process::exit(1);
-    }
+use clap::Parser;
 
-    let status = match std::env::args().nth(1) {
-        Some(s) => match root(&s) {
-            Ok((token, _)) => match generate_rand_string(&token) {
-                Ok(string) => {
-                    print!("{}", string);
-                    0
-                }
-                Err(_) => 1,
-            },
+#[derive(Parser)]
+#[command(name = "randstring")]
+#[command(author = "shirokurostone")]
+#[command(version = "0.0.1")]
+#[command(about = None, long_about = None)]
+struct Cli {
+    regexp: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let regexp = cli.regexp;
+
+    let status = match root(&regexp){
+        Ok((token, _)) => match generate_rand_string(&token) {
+            Ok(string) => {
+                print!("{}", string);
+                0
+            }
             Err(_) => 1,
         },
-        _ => 1,
+        Err(_) => 1,
     };
+
     std::process::exit(status);
 }
